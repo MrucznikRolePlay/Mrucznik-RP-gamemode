f785b9914b4562380b2354dd1d211529c407d97b
Mrucznik <mrucznix@gmail.com>
2018-05-07 20:28:01 +0200

    de3bug 10

--- CI_scripts/compile.sh
+++ CI_scripts/compile.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/compiler/lib
-~/compiler/bin/pawncc -iinclude -D_LINUX -D~/gamemodes -d3 "-;+" "-(+" $@
+echo "KROPKA:"
+ls .
+echo "HOME:"
+ls ~
+~/compiler/bin/pawncc -iinclude -d3 "-;+" "-(+" ~/gamemodes/Mrucznik-RP.pwn
