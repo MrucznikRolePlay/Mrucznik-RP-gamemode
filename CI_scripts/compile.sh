c1700fc349367357881639ff37952b9a25c2be33
Mrucznik <mrucznix@gmail.com>
2018-05-07 20:01:17 +0200

    de3bug 5

--- CI_scripts/compile.sh
+++ CI_scripts/compile.sh
@@ -4,4 +4,4 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/compiler/lib
 ls ~
 ls *
 ls */*
-~/compiler/bin/pawncc -iinclude -d3 "-;+" "-(+" $@
+~/compiler/bin/pawncc -iinclude -D_LINUX -D~/gamemodes -d3 "-;+" "-(+" $@
