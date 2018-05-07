709d435a61508e0263537468fc379f74938ddb04
Mrucznik <mrucznix@gmail.com>
2018-05-08 00:05:16 +0200

    CI fixes

--- CI_scripts/compile.sh
+++ CI_scripts/compile.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/compiler/lib
-echo "KROPKA:"
-ls .
-echo "HOME:"
-ls ~
 cd gamemodes
 ~/compiler/bin/pawncc -i../include -d3 "-;+" "-(+" Mrucznik-RP.pwn
