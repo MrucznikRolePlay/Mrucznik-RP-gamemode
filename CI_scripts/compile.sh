1aaae192a9afc3ff2d9472841593e2f3297d7589
Mrucznik <mrucznix@gmail.com>
2018-08-05 16:44:38 +0200

    Update compile.sh

--- CI_scripts/compile.sh
+++ CI_scripts/compile.sh
@@ -2,4 +2,4 @@
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/compiler/lib
 cd gamemodes
-~/compiler/bin/pawncc -i../include -d3 "-;+" "-(+" Mrucznik-RP.pwn
+~/compiler/pawncc -i../include -d3 "-;+" "-(+" Mrucznik-RP.pwn
