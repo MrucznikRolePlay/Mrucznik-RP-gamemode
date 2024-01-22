d3033f8c098a12284b536d5b1c6de0db63db89d5
Gumbalowsky <weremijowsky@gmail.com>
2024-01-23 00:37:20 +0100

    gitignore tweak + amx_fix update

--- amx_fix.sh
+++ amx_fix.sh
@@ -1,8 +1,13 @@
 
 #!/bin/bash
 cd dependencies
-rm -rf amx_assembly
+rm -r amx_assembly
 git clone git@github.com:Mrucznik/amx_assembly
 cd amx_assembly
 git checkout v4.69
-cd ../..
+cd ../
+rm -r indirection
+git clone git@github.com:Mrucznik/indirection
+
+
+
